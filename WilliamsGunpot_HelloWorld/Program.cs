c4ca8b281da0621d0a028fccefb71ebe1698eeb1
Jaaron-Gunpot <111777933+Jaaron-Gunpot@users.noreply.github.com>
2022-09-08 17:25:44 -0400

    Update Program.cs

--- WilliamsGunpot_HelloWorld/Program.cs
+++ WilliamsGunpot_HelloWorld/Program.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace WilliamsGunpot_HelloWorld
 {
+    //Class Program
+    //Author Jaaron Williams Gunpot
+    //Purpose make console say hello world
+    //restrictions none
     static internal class Program
     {
+        //Method Main
+        //Purpose make the console say hello world
+        //Restrictions none
         static void Main(string[] args)
         {
             //Make the computer say "Hello World!"
