590957bd19eead6c5253e165839b37548d48f731
Jaaron Williams Gunpot <jsw6194@rit.edu>
2022-09-20 16:22:53 -0400

    work in progress

---
+++ Raise Jaaron Gunpot/Raise Jaaron Gunpot/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raise_Jaaron_Gunpot
+{
+    static internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string sName;
+            double dSalary = 30000;
+
+            Console.WriteLine("What is your name?");
+            sName = Console.ReadLine();
+            if (GiveRaise(sName,ref dSalary))
+            {
+                Console.WriteLine("you got a raise");
+            }
+        }
+        static bool GiveRaise(string name, ref double salary)
+        {
+            return true;
+        }
+    }
+}
