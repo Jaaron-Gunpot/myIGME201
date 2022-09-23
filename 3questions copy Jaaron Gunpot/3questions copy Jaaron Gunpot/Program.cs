4b0eaeb0fa02f9612f598aeae8c154e61317f98d
Jaaron <jsw6194@rit.edu>
2022-09-23 13:05:28 -0400

    halfway

--- 3questions copy Jaaron Gunpot/3questions copy Jaaron Gunpot/Program.cs
+++ 3questions copy Jaaron Gunpot/3questions copy Jaaron Gunpot/Program.cs
@@ -4,66 +4,116 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
+using Timer = System.Timers.Timer;
 
 namespace _3questions_copy_Jaaron_Gunpot
 {
     static internal class Program
     {
+        static bool timeOut=false;
+
+        static Timer fiveSec;
         static void Main(string[] args)
         {
+
+            //if the time is finished or not
+            timeOut=false;
             //to check if the response is a valid input
             bool qChosen=false;
             //hold number chosen for question
             string nChoice = "";
             int nChosen=0;
-            //prompts for number of questions
-            do
-            {
-                Console.Write("Choose your question (1-3): ");
-                nChoice=Console.ReadLine();
-                try
+        //prompts for number of questions
+        begin:
+            
+                do
                 {
-                    nChosen=int.Parse(nChoice);
-                    if(nChosen==1|| nChosen == 2|| nChosen == 3)
+                    Console.Write("Choose your question (1-3): ");
+                    nChoice = Console.ReadLine();
+
+                    try
                     {
-                        qChosen = true;
+                        nChosen = int.Parse(nChoice);
+                        if (nChosen == 1 || nChosen == 2 || nChosen == 3)
+                        {
+                            qChosen = true;
+                        }
+
                     }
-                    
-                }
-                catch
-                {
-                    qChosen=false;
-                }
+                    catch
+                    {
+                        qChosen = false;
+                    }
 
-            }while (!qChosen);
-
-            //picks from a set of three questions based on user input
-            switch (nChosen)
+                } while (!qChosen);
+            while (!timeOut)
             {
-                case 1:
+                Timer fiveSec = new Timer(5000);
+                ElapsedEventHandler elapsedEventHandler = null;
+                elapsedEventHandler = new ElapsedEventHandler(timeUp);
+                fiveSec.Elapsed += elapsedEventHandler;
+                //picks from a set of three questions based on user input
+                switch (nChosen)
+                {
+                    case 1:
+                        Console.WriteLine("What is your favorite color?");
+                        break;
+                    case 2:
+                        Console.WriteLine("What is the awnser to life, the universe and everything?");
+                        break;
+                    case 3:
+                        Console.WriteLine("What is the airspeed velocity of an unladen swallow?");
+                        break;
+                }
+                //has a 5 second timer
+                fiveSec.Start();
+                string response = Console.ReadLine();
+                fiveSec.Stop();
+                //checks if response is correct
+                
+                /*switch (nChosen)
+                {
+                    case 1:
+                        if (response.ToLower() == "black")
+                        {
+                            Console.WriteLine("Well Done!");
+                        }else if(response.ToLower() != "black" || timeOut)
+                        {
+                            Console.WriteLine("The awnser is: black");
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("What is the awnser to life, the universe and everything?");
+                        break;
+                    case 3:
+                        Console.WriteLine("What is the airspeed velocity of an unladen swallow?");
+                        break;
+                }
+                //congratulates if answered correctly and provides awnser if wrong or out of time
+                //asks the user if they want to play again
 
-                    break;
+                /*
+                 * test code
+                 * start:
+                 *  string test;
+                 *  test=Console.ReadLine();
+                 *  if (test.ToLower() == "carrot")
+                 *  {
+                 *      Console.WriteLine("restart");
+                 *      goto start;
+                 *  }
+                 *  else
+                 *  {
+                 *      Console.WriteLine("Potato");
+                 *  }
+                 */
             }
-            //has a 5 second timer
-            //congratulates if answered correctly and provides awnser if wrong or out of time
-            //asks the user if they want to play again
-
-        /*
-         * test code
-         * start:
-         *  string test;
-         *  test=Console.ReadLine();
-         *  if (test.ToLower() == "carrot")
-         *  {
-         *      Console.WriteLine("restart");
-         *      goto start;
-         *  }
-         *  else
-         *  {
-         *      Console.WriteLine("Potato");
-         *  }
-         */
-
         }
+          static void timeUp(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Times up!");
+            timeOut = true;
+        }  
     }
 }
