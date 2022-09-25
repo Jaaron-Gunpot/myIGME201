1f4f14b51f955b51b7a2d712724ca63589606108
Jaaron <jsw6194@rit.edu>
2022-09-25 15:59:21 -0400

    I give up

--- 3questions copy Jaaron Gunpot/3questions copy Jaaron Gunpot/Program.cs
+++ 3questions copy Jaaron Gunpot/3questions copy Jaaron Gunpot/Program.cs
@@ -1,85 +1,94 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Timers;
-using Timer = System.Timers.Timer;
 
 namespace _3questions_copy_Jaaron_Gunpot
 {
     static internal class Program
     {
-        static bool timeOut=false;
+
 
         static Timer fiveSec;
-        static void Main(string[] args)
+        static public void Main(string[] args)
         {
+        begin:
             //if the time is finished or not
-            timeOut=false;
+            bool timeOut = false;
             //to check if the response is a valid input
-            bool qChosen=false;
+            bool qChosen = false;
             //hold number chosen for question
             string nChoice = "";
-            int nChosen=0;
+            string response = "";
+            int nChosen = 0;
         //prompts for number of questions
-        begin:
-            
-                do
+        
+
+            do
+            {
+                Console.Write("Choose your question (1-3): ");
+                nChoice = Console.ReadLine();
+
+                try
                 {
-                    Console.Write("Choose your question (1-3): ");
-                    nChoice = Console.ReadLine();
-
-                    try
+                    nChosen = int.Parse(nChoice);
+                    if (nChosen == 1 || nChosen == 2 || nChosen == 3)
                     {
-                        nChosen = int.Parse(nChoice);
-                        if (nChosen == 1 || nChosen == 2 || nChosen == 3)
-                        {
-                            qChosen = true;
-                        }
-
-                    }
-                    catch
-                    {
-                        qChosen = false;
+                        qChosen = true;
                     }
 
-                } while (!qChosen);
+                }
+                catch
+                {
+                    qChosen = false;
+                }
+
+            } while (!qChosen);
+            switch (nChosen)
+            {
+                case 1:
+                    Console.WriteLine("What is your favorite color?");
+                    break;
+                case 2:
+                    Console.WriteLine("What is the awnser to life, the universe and everything?");
+                    break;
+                case 3:
+                    Console.WriteLine("What is the airspeed velocity of an unladen swallow?");
+                    break;
+            }
             while (!timeOut)
             {
-                Timer fiveSec = new Timer(5000);
-                ElapsedEventHandler elapsedEventHandler = null;
-                elapsedEventHandler = new ElapsedEventHandler(timeUp);
-                fiveSec.Elapsed += elapsedEventHandler;
                 //picks from a set of three questions based on user input
+                
+                //has a 5 second timer
+                fiveSec = new Timer(5000);
+                //fiveSec.Elapsed+= new ElapsedEventHandler(timeUp);
+                fiveSec.Start();
+                fiveSec.Elapsed += delegate (object sender, ElapsedEventArgs e)
+                {
+                    //Console.WriteLine("");
+                    Console.WriteLine("Your time is up!");
+                    timeOut = true;
+                    fiveSec.Stop();
+                };
+                response = Console.ReadLine();
+
+                fiveSec.Stop();
+                //checks if response is correct
                 switch (nChosen)
                 {
                     case 1:
-                        Console.WriteLine("What is your favorite color?");
-                        break;
-                    case 2:
-                        Console.WriteLine("What is the awnser to life, the universe and everything?");
-                        break;
-                    case 3:
-                        Console.WriteLine("What is the airspeed velocity of an unladen swallow?");
-                        break;
-                }
-                //has a 5 second timer
-                fiveSec.Start();
-                string response = Console.ReadLine();
-                fiveSec.Stop();
-                //checks if response is correct
-                
-                /*switch (nChosen)
-                {
-                    case 1:
-                        if (response.ToLower() == "black")
+                        if (response.ToLower() == "black" && !timeOut)
                         {
                             Console.WriteLine("Well Done!");
-                        }else if(response.ToLower() != "black" || timeOut)
+                        }
+                        else if (response.ToLower() != "black" || timeOut)
                         {
                             Console.WriteLine("The awnser is: black");
+                            timeOut = true;
                         }
                         break;
                     case 2:
@@ -89,8 +98,6 @@ namespace _3questions_copy_Jaaron_Gunpot
                         Console.WriteLine("What is the airspeed velocity of an unladen swallow?");
                         break;
                 }
-                //congratulates if answered correctly and provides awnser if wrong or out of time
-                //asks the user if they want to play again
 
                 /*
                  * test code
@@ -108,11 +115,13 @@ namespace _3questions_copy_Jaaron_Gunpot
                  *  }
                  */
             }
+        end:
+            //asks the user if they want to play again
+            Console.Write("Press Enter to Play Again");
+            Console.ReadLine();
+            //congratulates if answered correctly and provides awnser if wrong or out of time
+            goto begin;
+
         }
-          static void timeUp(object sender, ElapsedEventArgs e)
-        {
-            Console.WriteLine("Times up!");
-            timeOut = true;
-        }  
     }
 }
