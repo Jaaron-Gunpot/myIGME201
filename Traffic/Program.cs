b2b8310e7f8f9fe9edd6a0378e5caba5f96ef01c
Jaaron <jsw6194@rit.edu>
2022-09-30 03:28:48 -0400

    Cars are vehicles too

---
+++ Traffic/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cars;
+
+namespace Traffic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPassengerCarrier passengerCarrier;
+            PassengerTrain passengerTrain = new PassengerTrain();
+            //passengerCarrier = PassengerTrain passengerCarrier1;
+            passengerCarrier = passengerTrain;
+            AddPassenger(passengerCarrier);
+            _424DoubleBogey bogey = new _424DoubleBogey();
+            //doesn't work
+            AddPassenger(bogey);
+
+        }
+        static void AddPassenger(IPassengerCarrier ThisVehicle)
+        {
+            ThisVehicle.LoadPassenger();
+            Console.WriteLine(ThisVehicle.ToString());
+        }
+    }
+}
