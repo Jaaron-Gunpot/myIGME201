9cdd26af94c97c0ae4bc2ab46c4a553edb31b634
Jaaron <jsw6194@rit.edu>
2022-10-05 16:22:44 -0400

    Hopeful done soon

---
+++ ClassTestBed/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTestBed
+{
+    public class Class1
+    {
+    }
+}
+public class MyClass
+{
+    private string myString;
+    public virtual string GetString()
+    {
+        return myString;
+    }
+    public string MyString
+    {
+        set
+        {
+            myString = value;
+        }
+    }
+
+}
+public class WriteString 
+{
+    private string myString;
+    
+}
+
+
---
+++ PE12-2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassTestBed;
+
+namespace PE12_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDerivedClass myDerived = new MyDerivedClass();
+            Console.WriteLine(myDerived.GetString());
+        }
+    }
+    public class MyDerivedClass : MyClass 
+    {
+        public override string GetString()
+        {
+            return base.GetString();
+        }
+    }
+
+}
