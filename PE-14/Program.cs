f4fa53c75f376325072d2b13d207bdcee6976d57
Jaaron <jsw6194@rit.edu>
2022-10-14 14:21:13 -0400

    all done

---
+++ PE-14/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_14
+{
+    internal class Program
+    {
+        public interface IFood
+        {
+            void Meal();
+        }
+        public class Mary : IFood
+        {
+            public void Meal()
+            {
+                Console.WriteLine("Mary eats Steak");
+            }
+        }
+        public class Alexis : IFood
+        {
+            public void Meal()
+            {
+                Console.WriteLine("Alexis eats Dragons");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Mary mary = new Mary();
+            Alexis alexis = new Alexis();
+            MyMethod(alexis);
+            MyMethod(mary);
+        }
+        public static void MyMethod(object myObject)
+        {
+            ((IFood)myObject).Meal();
+        }
+    }
+}
