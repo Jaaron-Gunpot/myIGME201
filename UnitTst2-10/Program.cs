8a943afb469d7bb77c7370f8a35f1054550e50e5
Jaaron <jsw6194@rit.edu>
2022-10-22 20:34:09 -0400

    done

---
+++ UnitTst2-10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using unitTst2_9;
+
+namespace UnitTst2_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Andre_Drummond andre = new Andre_Drummond();
+            Steph_Curry steph_ = new Steph_Curry();
+            MyMethod(steph_);
+            MyMethod(andre);
+            void MyMethod(object obj)
+            {
+                Console.WriteLine(((ISneakers)obj).Squeak("underarmor"));
+                Console.WriteLine(((ISneakers)obj).Squeak("nike"));
+            }
+        }
+    }
+}
