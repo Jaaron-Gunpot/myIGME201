5a6fd4f03fd9fd2dd201672d7c7fc69a7120a974
Jaaron Williams Gunpot <jsw6194@rit.edu>
2022-11-28 17:57:31 -0500

    rarity added again

--- Jaaron Stupid app/Jaaron Stupid app/Rarity.Designer.cs
+++ Jaaron Stupid app/Jaaron Stupid app/Rarity.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.spikeTextBox = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
@@ -41,7 +42,6 @@
             this.pictureBox10 = new System.Windows.Forms.PictureBox();
             this.pictureBox11 = new System.Windows.Forms.PictureBox();
             this.pictureBox12 = new System.Windows.Forms.PictureBox();
-            this.spikeTextBox = new System.Windows.Forms.TextBox();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -60,37 +60,47 @@
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 4;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 42.31884F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 57.68116F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 228F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
-            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 3, 0);
-            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 2, 0);
-            this.tableLayoutPanel1.Controls.Add(this.pictureBox3, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.pictureBox4, 2, 1);
-            this.tableLayoutPanel1.Controls.Add(this.pictureBox5, 3, 1);
-            this.tableLayoutPanel1.Controls.Add(this.pictureBox6, 3, 2);
-            this.tableLayoutPanel1.Controls.Add(this.pictureBox7, 2, 2);
-            this.tableLayoutPanel1.Controls.Add(this.pictureBox8, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.pictureBox9, 1, 2);
-            this.tableLayoutPanel1.Controls.Add(this.pictureBox10, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.pictureBox11, 0, 1);
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 199F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 101F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox3, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox4, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox5, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox6, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox7, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox8, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox9, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox10, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox11, 1, 2);
             this.tableLayoutPanel1.Controls.Add(this.pictureBox12, 0, 2);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 46);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 44);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 102F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(776, 392);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 112F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(776, 394);
             this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // spikeTextBox
+            // 
+            this.spikeTextBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.spikeTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.spikeTextBox.Location = new System.Drawing.Point(0, 0);
+            this.spikeTextBox.Name = "spikeTextBox";
+            this.spikeTextBox.Size = new System.Drawing.Size(800, 35);
+            this.spikeTextBox.TabIndex = 1;
+            this.spikeTextBox.Text = "Spike\'s Room";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(678, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(95, 139);
+            this.pictureBox1.Size = new System.Drawing.Size(232, 135);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -98,9 +108,9 @@
             // pictureBox2
             // 
             this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox2.Location = new System.Drawing.Point(450, 3);
+            this.pictureBox2.Location = new System.Drawing.Point(479, 144);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(222, 139);
+            this.pictureBox2.Size = new System.Drawing.Size(193, 135);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
@@ -108,9 +118,9 @@
             // pictureBox3
             // 
             this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox3.Location = new System.Drawing.Point(192, 3);
+            this.pictureBox3.Location = new System.Drawing.Point(678, 3);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(252, 139);
+            this.pictureBox3.Size = new System.Drawing.Size(95, 135);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 2;
             this.pictureBox3.TabStop = false;
@@ -118,9 +128,9 @@
             // pictureBox4
             // 
             this.pictureBox4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox4.Location = new System.Drawing.Point(450, 148);
+            this.pictureBox4.Location = new System.Drawing.Point(678, 144);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(222, 139);
+            this.pictureBox4.Size = new System.Drawing.Size(95, 135);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox4.TabIndex = 3;
             this.pictureBox4.TabStop = false;
@@ -128,9 +138,9 @@
             // pictureBox5
             // 
             this.pictureBox5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox5.Location = new System.Drawing.Point(678, 148);
+            this.pictureBox5.Location = new System.Drawing.Point(678, 285);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(95, 139);
+            this.pictureBox5.Size = new System.Drawing.Size(95, 106);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox5.TabIndex = 4;
             this.pictureBox5.TabStop = false;
@@ -138,9 +148,9 @@
             // pictureBox6
             // 
             this.pictureBox6.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox6.Location = new System.Drawing.Point(678, 293);
+            this.pictureBox6.Location = new System.Drawing.Point(479, 285);
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(95, 96);
+            this.pictureBox6.Size = new System.Drawing.Size(193, 106);
             this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox6.TabIndex = 5;
             this.pictureBox6.TabStop = false;
@@ -148,9 +158,9 @@
             // pictureBox7
             // 
             this.pictureBox7.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox7.Location = new System.Drawing.Point(450, 293);
+            this.pictureBox7.Location = new System.Drawing.Point(479, 3);
             this.pictureBox7.Name = "pictureBox7";
-            this.pictureBox7.Size = new System.Drawing.Size(222, 96);
+            this.pictureBox7.Size = new System.Drawing.Size(193, 135);
             this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox7.TabIndex = 6;
             this.pictureBox7.TabStop = false;
@@ -158,9 +168,9 @@
             // pictureBox8
             // 
             this.pictureBox8.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox8.Location = new System.Drawing.Point(192, 148);
+            this.pictureBox8.Location = new System.Drawing.Point(241, 3);
             this.pictureBox8.Name = "pictureBox8";
-            this.pictureBox8.Size = new System.Drawing.Size(252, 139);
+            this.pictureBox8.Size = new System.Drawing.Size(232, 135);
             this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox8.TabIndex = 7;
             this.pictureBox8.TabStop = false;
@@ -168,9 +178,9 @@
             // pictureBox9
             // 
             this.pictureBox9.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox9.Location = new System.Drawing.Point(192, 293);
+            this.pictureBox9.Location = new System.Drawing.Point(241, 144);
             this.pictureBox9.Name = "pictureBox9";
-            this.pictureBox9.Size = new System.Drawing.Size(252, 96);
+            this.pictureBox9.Size = new System.Drawing.Size(232, 135);
             this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox9.TabIndex = 8;
             this.pictureBox9.TabStop = false;
@@ -178,9 +188,9 @@
             // pictureBox10
             // 
             this.pictureBox10.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox10.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox10.Location = new System.Drawing.Point(3, 144);
             this.pictureBox10.Name = "pictureBox10";
-            this.pictureBox10.Size = new System.Drawing.Size(183, 139);
+            this.pictureBox10.Size = new System.Drawing.Size(232, 135);
             this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox10.TabIndex = 9;
             this.pictureBox10.TabStop = false;
@@ -188,9 +198,9 @@
             // pictureBox11
             // 
             this.pictureBox11.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox11.Location = new System.Drawing.Point(3, 148);
+            this.pictureBox11.Location = new System.Drawing.Point(241, 285);
             this.pictureBox11.Name = "pictureBox11";
-            this.pictureBox11.Size = new System.Drawing.Size(183, 139);
+            this.pictureBox11.Size = new System.Drawing.Size(232, 106);
             this.pictureBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox11.TabIndex = 10;
             this.pictureBox11.TabStop = false;
@@ -198,24 +208,12 @@
             // pictureBox12
             // 
             this.pictureBox12.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox12.Location = new System.Drawing.Point(3, 293);
+            this.pictureBox12.Location = new System.Drawing.Point(3, 285);
             this.pictureBox12.Name = "pictureBox12";
-            this.pictureBox12.Size = new System.Drawing.Size(183, 96);
+            this.pictureBox12.Size = new System.Drawing.Size(232, 106);
             this.pictureBox12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox12.TabIndex = 11;
             this.pictureBox12.TabStop = false;
-            // 
-            // spikeTextBox
-            // 
-            this.spikeTextBox.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.spikeTextBox.Dock = System.Windows.Forms.DockStyle.Top;
-            this.spikeTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.spikeTextBox.Location = new System.Drawing.Point(0, 0);
-            this.spikeTextBox.Name = "spikeTextBox";
-            this.spikeTextBox.ReadOnly = true;
-            this.spikeTextBox.Size = new System.Drawing.Size(800, 35);
-            this.spikeTextBox.TabIndex = 1;
-            this.spikeTextBox.Text = "Spike\'s Room";
             // 
             // Rarity
             // 
@@ -247,6 +245,7 @@
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox spikeTextBox;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
@@ -259,6 +258,5 @@
         private System.Windows.Forms.PictureBox pictureBox10;
         private System.Windows.Forms.PictureBox pictureBox11;
         private System.Windows.Forms.PictureBox pictureBox12;
-        private System.Windows.Forms.TextBox spikeTextBox;
     }
 }
