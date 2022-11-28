45a33c17b3fdc6b4caee2af54ed4ff8c04f9bc98
Jaaron <jsw6194@rit.edu>
2022-11-28 11:56:37 -0500

    b

--- Jaaron Stupid app/Jaaron Stupid app/PinkiePie.Designer.cs
+++ Jaaron Stupid app/Jaaron Stupid app/PinkiePie.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PinkiePie));
             this.ponyPictureBox = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.ponyPictureBox)).BeginInit();
             this.SuspendLayout();
@@ -35,18 +36,22 @@
             // ponyPictureBox
             // 
             this.ponyPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ponyPictureBox.InitialImage = ((System.Drawing.Image)(resources.GetObject("ponyPictureBox.InitialImage")));
             this.ponyPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.ponyPictureBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ponyPictureBox.Name = "ponyPictureBox";
-            this.ponyPictureBox.Size = new System.Drawing.Size(800, 450);
+            this.ponyPictureBox.Size = new System.Drawing.Size(533, 292);
+            this.ponyPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.ponyPictureBox.TabIndex = 0;
             this.ponyPictureBox.TabStop = false;
             // 
             // PinkiePie
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(533, 292);
             this.Controls.Add(this.ponyPictureBox);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "PinkiePie";
             this.Text = "PinkiePie";
             ((System.ComponentModel.ISupportInitialize)(this.ponyPictureBox)).EndInit();
