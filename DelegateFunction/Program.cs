5c7fdadc6553638ca3598e96131b8c8008815d17
Jaaron <jsw6194@rit.edu>
2022-12-13 14:17:49 -0500

    l

--- DelegateFunction/Program.cs
+++ DelegateFunction/Program.cs
@@ -17,22 +17,13 @@ namespace DelegateFunction
             // your code here
             myRounder = new MyRounder(Math.Round);
             myRounder = Math.Round;
-            myRounder = delegate (double d, int n)
-            {
-                return Math.Round(d, n);
-            };
-            myRounder = (double d, int n) =>
-            {
-                return Math.Round(d, n);
-            };
-            myRounder = (d, n) =>
-            {
-                return Math.Round(d, n);
-            };
+            myRounder = delegate (double d, int n){return Math.Round(d, n);};
+            myRounder = (double d, int n) =>{return Math.Round(d, n);};
+            myRounder = (d, n) =>{return Math.Round(d, n);};
             myRounder = (d, n) => Math.Round(d, n);
-
+            myRounder = (double d, int n) => Math.Round(d, n);
             Func<double, int, double> rounder = Math.Round;
-            rounder = (d, n) => (double)Math.Round(d, n);
+            //rounder = (d, n) => (double)Math.Round(d, n);
 
             //Console.WriteLine(rounder(4.5642,1).ToString());
         }
